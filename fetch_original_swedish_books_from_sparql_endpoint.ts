39963484dc1a3e5b45a93093b3e38df04a80692a
Qluxzz <andre.eric.andersson@gmail.com>
2025-10-23 21:51:01 +0200

    Tweak what genres we accept

--- fetch_original_swedish_books_from_sparql_endpoint.ts
+++ fetch_original_swedish_books_from_sparql_endpoint.ts
@@ -96,9 +96,20 @@ export enum Type {
 }
 
 const invalidGenres = new Set([
-  "https://id.kb.se/marc/Poetry",
-  "https://id.kb.se/term/saogf/Poesi",
+  "https://id.kb.se/marc/ComicOrGraphicNovel",
+  "https://id.kb.se/marc/ComicStrip",
+  "https://id.kb.se/marc/Encyclopedia",
+  "https://id.kb.se/marc/Essay",
   "https://id.kb.se/marc/NotFictionNotFurtherSpecified",
+  "https://id.kb.se/marc/Poetry",
+  "https://id.kb.se/marc/Yearbook",
+  "https://id.kb.se/term/gmgpc/swe/Tecknade%20serier",
+  "https://id.kb.se/term/saogf/Allegorier",
+  "https://id.kb.se/term/saogf/Bildverk",
+  "https://id.kb.se/term/saogf/Ljudb%C3%B6cker",
+  "https://id.kb.se/term/saogf/Ljudbearbetningar",
+  "https://id.kb.se/term/saogf/Poesi",
+  "https://id.kb.se/term/saogf/Tecknade%20serier",
 ])
 
 async function findTitlesPublishedInYear(year: number): Promise<Release[]> {
@@ -106,48 +117,53 @@ async function findTitlesPublishedInYear(year: number): Promise<Release[]> {
   const parse = JSON.parse(data) as SparqlResponse
 
   // We get one row per genre of the work
-  return (
-    parse.results.bindings
-      .reduce<{ invalid: Set<string>; valid: Map<string, Release> }>(
-        (acc, x) => {
-          const { invalid, valid } = acc
+  return parse.results.bindings
+    .reduce<{ invalid: Set<string>; valid: Map<string, Release> }>(
+      (acc, x) => {
+        const { invalid, valid } = acc
 
-          if (invalid.has(x.work.value)) return acc
+        if (invalid.has(x.work.value)) return acc
 
-          // Ignore works without a valid reference
-          if (x.work.type === Type.Bnode) {
-            invalid.add(x.work.value)
-            return acc
-          }
+        // Ignore works without a valid reference
+        if (x.work.type === Type.Bnode) {
+          invalid.add(x.work.value)
+          return acc
+        }
 
-          if (invalidGenres.has(x.gf.value)) {
-            invalid.add(x.work.value)
-            // Since we get one row per genre we might have added a previous instance
-            // before we know it was invalid, so we remove it
-            valid.delete(x.work.value)
+        // Skip adding if genre is bNode, if it shows up under another genre, we will add it then
+        if (x.gf.type === Type.Bnode) return acc
 
-            return acc
-          }
+        // Ignore all children's books
+        if (x.gf.value.startsWith("https://id.kb.se/term/barngf")) {
+          invalid.add(x.work.value)
+          return acc
+        }
 
-          const existing = valid.get(x.work.value)
-          if (existing) existing.genres.add(x.gf.value)
-          else
-            valid.set(x.work.value, {
-              title: x.title.value,
-              author: `${x.givenName.value} ${x.familyName.value}`,
-              genres: new Set<string>([x.gf.value]),
-              isbn: x.isbn?.value,
-            })
+        if (invalidGenres.has(x.gf.value)) {
+          invalid.add(x.work.value)
+          // Since we get one row per genre we might have added a previous instance
+          // before we know it was invalid, so we remove it
+          valid.delete(x.work.value)
 
           return acc
-        },
-        { invalid: new Set<string>(), valid: new Map() }
-      )
-      .valid.values()
-      // Ignore any title that only has a single genre of some node id
-      .filter((x) => [...x.genres].some((g) => !g.startsWith("nodeID://b")))
-      .toArray()
-  )
+        }
+
+        const existing = valid.get(x.work.value)
+        if (existing) existing.genres.add(x.gf.value)
+        else
+          valid.set(x.work.value, {
+            title: x.title.value,
+            author: `${x.givenName.value} ${x.familyName.value}`,
+            genres: new Set<string>([x.gf.value]),
+            isbn: x.isbn?.value,
+          })
+
+        return acc
+      },
+      { invalid: new Set<string>(), valid: new Map() }
+    )
+    .valid.values()
+    .toArray()
 }
 
 function makeSetsSerializable(_: string, value: any) {
@@ -167,16 +183,17 @@ const END_YEAR = 2024
 // so we start 20 requests in parallel
 const limit = pLimit(20)
 
-const tasks = [...Array(END_YEAR - STARTING_YEAR)].map((_, i) =>
+const tasks = [...Array(END_YEAR - STARTING_YEAR + 1)].map((_, i) =>
   limit(async function () {
     const year = STARTING_YEAR + i
 
-    console.log(`Fetching releases for year ${year}`)
+    console.log(`${year}: Fetching titles`)
     const result = await findTitlesPublishedInYear(year)
+    console.log(`${year}: Try to add Goodreads data`)
     const enhanced = await enhanceReleaseWithDataFromGoodReads(result)
 
     console.log(
-      `Found ${enhanced.length} releases, of which ${hasGoodReadsData(
+      `${year}: Found ${enhanced.length} releases, of which ${hasGoodReadsData(
         enhanced
       )} releases were found on Goodread`
     )
