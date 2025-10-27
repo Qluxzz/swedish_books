52dfbb464e9a5828424af9b3459e28c3d0b39a87
Qluxzz <andre.eric.andersson@gmail.com>
2025-10-27 22:43:56 +0100

    Rework ranking

--- frontend/custom-backend-task.ts
+++ frontend/custom-backend-task.ts
@@ -1,5 +1,39 @@
 import { DatabaseSync } from "node:sqlite"
 
+const ranked = `
+ranked AS (
+  SELECT 
+    id,
+    pct
+  FROM (
+    SELECT
+      id,
+      PERCENT_RANK() OVER (ORDER BY ratings ASC) AS pct
+    FROM books
+    WHERE ratings > 0
+  ) WHERE pct < 0.98
+)
+`
+
+const filterPopularAuthors = `
+popularity AS (
+  SELECT 
+    author,
+    lifeSpan,
+    pct
+  FROM (
+    SELECT
+      author,
+      lifeSpan,
+      PERCENT_RANK() OVER (ORDER BY SUM(instances) ASC) AS pct
+    FROM books GROUP BY author, lifeSpan
+  ) WHERE pct < 0.955
+)`
+
+const database = new DatabaseSync("../books6.db", {
+  readOnly: true,
+})
+
 export async function getRatedTitles() {
   console.log("Get titles")
   const database = new DatabaseSync("../books6.db")
@@ -7,26 +41,27 @@ export async function getRatedTitles() {
   const result = database
     .prepare(
       `
-WITH ranked AS (
-  SELECT
-    *,
-    PERCENT_RANK() OVER (ORDER BY ratings DESC) AS pct
-  FROM books
-  WHERE ratings > 10 AND year < 2000
-)
+WITH ${ranked}, ${filterPopularAuthors}
 SELECT
-  title,
-  author,
-  lifeSpan,
-  year,
-  isbn,
-  avgRating,
-  ratings,
-  bookUrl,
-  imageId
-FROM ranked
-WHERE pct > 0.25 AND pct < 0.75 AND avgRating > 3
-ORDER BY random()
+  b.title,
+  b.author,
+  b.lifeSpan,
+  b.year,
+  b.isbn,
+  b.avgRating,
+  b.ratings,
+  b.bookUrl,
+  b.imageId
+FROM books b
+INNER JOIN popularity p
+  ON p.author = b.author
+  AND p.lifeSpan = b.lifeSpan
+INNER JOIN ranked r
+  ON r.id = b.id
+  AND r.pct < 0.8
+-- Needs more tweaking, I would like to highlight older titles, but not have a hard cutoff
+WHERE b.year < 2010 AND b.avgRating > 3 AND b.ratings > 10
+ORDER BY b.year DESC
 LIMIT 24;
     `
     )
@@ -36,20 +71,22 @@ LIMIT 24;
 }
 
 export async function getUnratedTitles() {
-  const database = new DatabaseSync("../books6.db")
-
   const result = database
     .prepare(
       `
+WITH ${filterPopularAuthors}
 SELECT
-  title,
-  author,
-  lifeSpan,
-  year
-FROM books
-WHERE ratings = 0
-ORDER BY random()
-LIMIT 24;
+  b.title,
+  b.author,
+  b.lifeSpan,
+  b.year
+FROM books b
+INNER JOIN popularity p
+  ON p.author = b.author
+  AND p.lifeSpan = b.lifeSpan
+WHERE b.ratings IS NULL
+ORDER BY p.pct DESC, RANDOM()
+LIMIT 24
     `
     )
     .all()
@@ -58,31 +95,29 @@ LIMIT 24;
 }
 
 export async function getTitlesForYear(year: string) {
-  const database = new DatabaseSync("../books6.db")
-
   const ratedTitles = database
     .prepare(
       `
-WITH ranked AS (
-  SELECT
-    *,
-    PERCENT_RANK() OVER (ORDER BY ratings DESC) AS pct
-  FROM books
-  WHERE ratings > 10 AND year = ?
-)
+WITH ${ranked}, ${filterPopularAuthors}
 SELECT
-  title,
-  author,
-  lifeSpan,
-  year,
-  isbn,
-  avgRating,
-  ratings,
-  bookUrl,
-  imageId
-FROM ranked
-WHERE pct > 0.25 AND pct < 0.75 AND avgRating > 3
-ORDER BY random()
+  b.title,
+  b.author,
+  b.lifeSpan,
+  b.year,
+  b.isbn,
+  b.avgRating,
+  b.ratings,
+  b.bookUrl,
+  b.imageId
+FROM books b
+INNER JOIN ranked r
+  ON b.id = r.id
+INNER JOIN popularity p
+  ON p.author = b.author
+  AND p.lifeSpan = b.lifeSpan
+WHERE 
+  b.year = ?
+ORDER BY b.avgRating DESC
 LIMIT 24;
     `
     )
@@ -91,18 +126,62 @@ LIMIT 24;
   const unratedTitles = database
     .prepare(
       `
+WITH ${filterPopularAuthors}
 SELECT
-  title,
-  author,
-  lifeSpan,
-  year
-FROM books
-WHERE ratings = 0 AND year = ?
-ORDER BY random()
-LIMIT 24;
+  b.title,
+  b.author,
+  b.lifeSpan,
+  b.year
+FROM books b
+INNER JOIN popularity p
+  ON p.author = b.author
+  AND p.lifeSpan = b.lifeSpan
+WHERE (b.ratings IS NULL OR b.ratings = 0) AND b.year = ?
+ORDER BY p.pct DESC, RANDOM()
+LIMIT 48
     `
     )
     .all(year)
 
   return { ratedTitles, unratedTitles }
+}
+
+export function getCountOfTitlesPerYear() {
+  return database
+    .prepare(
+      `
+WITH ${ranked}, ${filterPopularAuthors}
+SELECT 
+  year,
+  SUM(count) amount
+FROM (
+SELECT
+  b.year,
+  COUNT(*) count
+FROM books b
+INNER JOIN ranked r
+  ON b.id = r.id
+INNER JOIN popularity p
+  ON p.author = b.author
+  AND p.lifeSpan = b.lifeSpan
+GROUP BY b.year
+UNION ALL
+SELECT
+  b.year,
+  COUNT(*) count
+FROM books b
+INNER JOIN popularity p
+  ON p.author = b.author
+  AND p.lifeSpan = b.lifeSpan
+WHERE b.ratings IS NULL
+GROUP BY b.year
+)
+GROUP BY year
+`
+    )
+    .all()
+    .reduce((acc, row) => {
+      acc[row.year] = row.amount
+      return acc
+    }, {})
 }
