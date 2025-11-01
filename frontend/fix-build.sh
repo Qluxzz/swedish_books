a89f831101acd0524e2df807e1067936fe261dee
Qluxzz <andre.eric.andersson@gmail.com>
2025-11-01 12:13:46 +0100

    Update image assets to use correct base path

--- frontend/fix-build.sh
+++ frontend/fix-build.sh
@@ -10,10 +10,12 @@ find dist/ -type f -name "*.html" | while read -r file; do
   # 1. Remove all <script>...</script> blocks
   # 2. Remove all <link rel="modulepreload" ...> tags
   # 3. Update all href="/ to href="/swedish_books/
+  # 4. Update src="/ to src="/swedish_books/
   sed -E \
     -e 's#<script[^<]*</script>##g' \
     -e 's#<link rel="modulepreload"[^>]*>##g' \
     -e "s#href=\"/+#href=\"${BASE_PATH}/#g" \
+    -e "s#src=\"/+#src=\"${BASE_PATH}/#g" \
     "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
 done
 
