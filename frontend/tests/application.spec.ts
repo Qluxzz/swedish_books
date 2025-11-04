cae8d698f542766fdc9257ac267e4408924e01ad
Qluxzz <andre.eric.andersson@gmail.com>
2025-11-04 15:58:54 +0100

    Add tests for pagination and rated and unrated pages

--- frontend/tests/application.spec.ts
+++ frontend/tests/application.spec.ts
@@ -168,55 +168,158 @@ test.describe("application", () => {
     }
   })
 
-  test("Sorting is stable on a rated books page", async ({ page }) => {
-    await page.goto("/betygsatt/1")
-    const books = await Promise.all(
-      (await page.locator(".book-card").all()).map((x) => x.textContent())
-    )
-    await page.reload()
-    const books2 = await Promise.all(
-      (await page.locator(".book-card").all()).map((x) => x.textContent())
-    )
-
-    expect(books).toMatchObject(books2)
-  })
-
-  test("Sorting is stable on a unrated books page", async ({ page }) => {
-    await page.goto("/ej-betygsatt/1")
-    const books = await Promise.all(
-      (await page.locator(".book-card").all()).map((x) => x.textContent())
-    )
-    await page.reload()
-    const books2 = await Promise.all(
-      (await page.locator(".book-card").all()).map((x) => x.textContent())
-    )
-
-    expect(books).toMatchObject(books2)
-  })
-
-  test("Pagination on rated titles works", async ({ page }) => {
-    await page.goto("/betygsatt/1")
-    await page
-      .locator(".page-selector")
-      .getByRole("link", { name: "2", exact: true })
-      .click()
-    expect(page.url()).toContain("/betygsatt/2")
-  })
-
-  test("Pagination on unrated titles works", async ({ page }) => {
-    await page.goto("/ej-betygsatt/1")
-    await page
-      .locator(".page-selector")
-      .getByRole("link", { name: "2", exact: true })
-      .click()
-    expect(page.url()).toContain("/ej-betygsatt/2")
-  })
-
-  // HELPERS
-
-  function findBook(title: string, authorAndLifeSpan: string, page: Page) {
-    return page.locator(".book-card", {
-      hasText: `${title}${authorAndLifeSpan}`,
+  test.describe("Rated books", () => {
+    test("Navigating to rated tiles works", async ({ page }) => {
+      await page
+        .getByRole("link", { name: "Se alla betygsatta böcker" })
+        .click()
+      expect(page.url()).toContain("/betygsatt/1")
+      await expect(
+        page.getByRole("heading", {
+          level: 1,
+          name: "Visar alla betygsatta böcker",
+        })
+      ).toBeVisible()
     })
-  }
+
+    test("Books on home page are not duplicated on first page of rated books", async ({
+      page,
+    }) => {
+      const section = page.locator("section", {
+        has: page.getByRole("heading", {
+          level: 2,
+          name: "Betygsatta böcker",
+        }),
+      })
+      const books = await Promise.all(
+        (await section.locator(".book-card").all()).map((x) => x.textContent())
+      )
+
+      await section
+        .getByRole("link", { name: "Se alla betygsatta böcker" })
+        .click()
+
+      await expect(
+        page.getByRole("heading", {
+          level: 1,
+          name: "Visar alla betygsatta böcker",
+        })
+      ).toBeVisible()
+
+      const booksFromPage2 = await Promise.all(
+        (await page.locator(".book-card").all()).map((x) => x.textContent())
+      )
+      expect(books).not.toMatchObject(booksFromPage2)
+    })
+
+    test("Sorting is stable on a rated books page", async ({ page }) => {
+      await page.goto("/betygsatt/1")
+      const books = await Promise.all(
+        (await page.locator(".book-card").all()).map((x) => x.textContent())
+      )
+      await page.reload()
+      const books2 = await Promise.all(
+        (await page.locator(".book-card").all()).map((x) => x.textContent())
+      )
+
+      expect(books).toMatchObject(books2)
+    })
+
+    test("Pagination on rated titles works", async ({ page }) => {
+      await page.goto("/betygsatt/1")
+      await page
+        .locator(".page-selector")
+        .getByRole("link", { name: "2", exact: true })
+        .click()
+      expect(page.url()).toContain("/betygsatt/2")
+    })
+
+    test("Last page of rated titles has titles", async ({ page }) => {
+      await page.goto("/betygsatt/1")
+      await page.locator(".page-selector").getByRole("link").last().click()
+
+      expect(await page.locator(".book-card").count()).not.toBe(0)
+    })
+  })
+
+  test.describe("Unrated books", () => {
+    test("Navigating to unrated tiles works", async ({ page }) => {
+      await page
+        .getByRole("link", { name: "Se alla ej betygsatta böcker" })
+        .click()
+      expect(page.url()).toContain("/ej-betygsatt/1")
+      await expect(
+        page.getByRole("heading", {
+          level: 1,
+          name: "Visar alla ej betygsatta böcker",
+        })
+      ).toBeVisible()
+    })
+
+    test("Books on home page are not duplicated on first page of unrated books", async ({
+      page,
+    }) => {
+      const section = page.locator("section", {
+        has: page.getByRole("heading", {
+          level: 2,
+          name: "Mysterierna",
+        }),
+      })
+      const books = await Promise.all(
+        (await section.locator(".book-card").all()).map((x) => x.textContent())
+      )
+
+      await section
+        .getByRole("link", { name: "Se alla ej betygsatta böcker" })
+        .click()
+
+      await expect(
+        page.getByRole("heading", {
+          level: 1,
+          name: "Visar alla ej betygsatta böcker",
+        })
+      ).toBeVisible()
+
+      const booksFromPage2 = await Promise.all(
+        (await page.locator(".book-card").all()).map((x) => x.textContent())
+      )
+      expect(books).not.toMatchObject(booksFromPage2)
+    })
+
+    test("Sorting is stable on a unrated books page", async ({ page }) => {
+      await page.goto("/ej-betygsatt/1")
+      const books = await Promise.all(
+        (await page.locator(".book-card").all()).map((x) => x.textContent())
+      )
+      await page.reload()
+      const books2 = await Promise.all(
+        (await page.locator(".book-card").all()).map((x) => x.textContent())
+      )
+
+      expect(books).toMatchObject(books2)
+    })
+
+    test("Pagination on unrated titles works", async ({ page }) => {
+      await page.goto("/ej-betygsatt/1")
+      await page
+        .locator(".page-selector")
+        .getByRole("link", { name: "2", exact: true })
+        .click()
+      expect(page.url()).toContain("/ej-betygsatt/2")
+    })
+
+    test("Last page of unrated titles has titles", async ({ page }) => {
+      await page.goto("/ej-betygsatt/1")
+      await page.locator(".page-selector").getByRole("link").last().click()
+
+      expect(await page.locator(".book-card").count()).not.toBe(0)
+    })
+  })
 })
+
+// HELPERS
+function findBook(title: string, authorAndLifeSpan: string, page: Page) {
+  return page.locator(".book-card", {
+    hasText: `${title}${authorAndLifeSpan}`,
+  })
+}
