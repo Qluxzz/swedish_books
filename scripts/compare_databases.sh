f96d8845cba65946d176fb0ab211270e7415a499
Qluxzz <andre.eric.andersson@gmail.com>
2025-11-19 14:59:54 +0100

    Update compare databases to just show number of records

--- scripts/compare_databases.sh
+++ scripts/compare_databases.sh
@@ -10,48 +10,55 @@ fi
 sqlite3 :memory: "attach \"$1\" as old; attach \"$2\" as new; 
 SELECT 'Books that does not exist in both database';
 SELECT 'Old', 'New';
-SELECT * FROM (
-    SELECT 
-        ob.title, 
-        oa.name 
-    FROM 
-        old.books ob
-    INNER JOIN
-        old.authors oa ON oa.id = ob.author_id
-) ob FULL OUTER JOIN (
-    SELECT 
-        nb.title, 
-        na.name
-    FROM
-        new.books nb 
-    INNER JOIN 
-        new.authors na ON na.id = nb.author_id 
-) nb ON
-    nb.title = ob.title 
-    AND nb.name = ob.name
-WHERE nb.title is NULL OR ob.title IS NULL;
+SELECT (SELECT COUNT(*) FROM old.books), (SELECT COUNT(*) FROM new.books);
+
+-- Prints the books that don't exist in both databases
+-- SELECT * FROM (
+--     SELECT 
+--         ob.title, 
+--         oa.name 
+--     FROM 
+--         old.books ob
+--     INNER JOIN
+--         old.authors oa ON oa.id = ob.author_id
+-- ) ob FULL OUTER JOIN (
+--     SELECT 
+--         nb.title, 
+--         na.name
+--     FROM
+--         new.books nb 
+--     INNER JOIN 
+--         new.authors na ON na.id = nb.author_id 
+-- ) nb ON
+--     nb.title = ob.title 
+--     AND nb.name = ob.name
+-- WHERE nb.title is NULL OR ob.title IS NULL;
 
 SELECT '';
 
 SELECT 'Authors that does not exist in both database';
 SELECT 'Old', 'New';
-SELECT * FROM (
-    SELECT
-        a.name,
-        a.life_span 
-    FROM
-        old.authors a
-) oa FULL OUTER JOIN (
-    SELECT 
-        a.name,
-        a.life_span 
-    FROM 
-        new.authors a 
-) na ON
-    na.name = oa.name 
-    AND na.life_span = oa.life_span
-WHERE na.name is NULL OR oa.name IS NULL
-ORDER by oa.name ASC, na.name ASC;
+SELECT (SELECT COUNT(*) FROM old.authors), (SELECT COUNT(*) FROM new.authors);
+
+
+-- Prints the authors that don't exist in both databases
+-- SELECT * FROM (
+--     SELECT
+--         a.name,
+--         a.life_span 
+--     FROM
+--         old.authors a
+-- ) oa FULL OUTER JOIN (
+--     SELECT 
+--         a.name,
+--         a.life_span 
+--     FROM 
+--         new.authors a 
+-- ) na ON
+--     na.name = oa.name 
+--     AND na.life_span = oa.life_span
+-- WHERE na.name is NULL OR oa.name IS NULL
+-- ORDER by oa.name ASC, na.name ASC;
 
 SELECT '';
 SELECT 'Book covers';
